c09554b36d8eb6ccd9cc90f3bdcb25ca45672776
Peter <ntrongphuc.1302@gmail.com>
2024-01-14 18:45:32 +0700

    main

--- index.js
+++ index.js
@@ -4,12 +4,7 @@ require("dotenv").config();
 const bot = new Eris(process.env.token);
 
 bot.on("ready", () => {
-  // Only work for bots
   console.log(`Logged in as ${bot.user.username} (${bot.user.id})`);
-  bot.editStatus("online", {
-    name: "Copilot",
-    type: 0, // 0: Playing, 1: Streaming, 2: Listening, 3: Watching, 4: Custom
-  });
 });
 
 bot.on("error", (err) => {
