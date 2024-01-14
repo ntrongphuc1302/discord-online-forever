f932d261937d335902fe0771abc383a3b0ad7105
Peter <ntrongphuc.1302@gmail.com>
2024-01-14 10:24:24 +0700

    main

--- index.js
+++ index.js
@@ -5,9 +5,7 @@ const bot = new Eris(process.env.token);
 
 bot.on("ready", () => {
   // Only work for bots
-  console.log(
-    `Logged in as ${bot.user.username}#${bot.user.discriminator} (${bot.user.id})`
-  );
+  console.log(`Logged in as ${bot.user.username} (${bot.user.id})`);
   bot.editStatus("online", {
     name: "Copilot",
     type: 0, // 0: Playing, 1: Streaming, 2: Listening, 3: Watching, 4: Custom
